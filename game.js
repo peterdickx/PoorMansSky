93394b7e0dad7f058d657f132353fdf4fb952176
Peter Dickx <peter.dickx@ehb.be>
2020-10-27 14:32:17 +0100

    changed size of buttons

--- game.js
+++ game.js
@@ -30,6 +30,14 @@ window.onkeydown = keyPressed;
 
 window.ontouchstart = touch;
 
+window.mobileAndTabletCheck = function () {
+    let check = false;
+    (function (a) {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+};
+
 let width = context.canvas.width;
 let height = context.canvas.height;
 
@@ -122,21 +130,21 @@ function touch(eventData) {
         reset();
         let touchX = eventData.touches[0].pageX;
         let touchY = eventData.touches[0].pageY;
-        if (Utils.calculateDistance(touchX, touchY, width - 35, height - 100) < 35) {
+        if (Utils.calculateDistance(touchX, touchY, width - 50, height - 125) < 35) {
             if (xSpeed < xMaxSpeed) {
                 xSpeed += 0.005;
             }
-        } else if (Utils.calculateDistance(touchX, touchY, width - 135, height - 100) < 35) {
+        } else if (Utils.calculateDistance(touchX, touchY, width - 200, height - 125) < 35) {
             if (xSpeed > -xMaxSpeed) {
                 xSpeed -= 0.005;
             }
         }
 
-        if (Utils.calculateDistance(touchX, touchY, width - 85, height - 150) < 35) {
+        if (Utils.calculateDistance(touchX, touchY, width - 125, height - 50) < 35) {
             if (ySpeed < yMaxSpeed) {
                 ySpeed += 1;
             }
-        } else if (Utils.calculateDistance(touchX, touchY, width - 85, height - 50) < 35) {
+        } else if (Utils.calculateDistance(touchX, touchY, width - 125, height - 200) < 35) {
             if (ySpeed > -yMaxSpeed) {
                 ySpeed -= 1;
             }
@@ -215,15 +223,15 @@ function draw() {
 
         //UI
         context.fillStyle = "darkblue";
-        Utils.fillCircle(width - 35, height - 100, 35);
-        Utils.fillCircle(width - 85, height - 150, 35);
-        Utils.fillCircle(width - 85, height - 50, 35);
-        Utils.fillCircle(width - 135, height - 100, 35);
+        Utils.fillCircle(width - 50, height - 125, 50);
+        Utils.fillCircle(width - 125, height - 200, 50);
+        Utils.fillCircle(width - 125, height - 50, 50);
+        Utils.fillCircle(width - 200, height - 125, 50);
         context.fillStyle = "white";
-        Utils.fillTriangle(width - 60, height - 135, width - 110, height - 135, width - 85, height - 180);
-        Utils.fillTriangle(width - 50, height - 75, width - 50, height - 125, width - 5, height - 100);
-        Utils.fillTriangle(width - 60, height - 65, width - 110, height - 65, width - 85, height - 20);
-        Utils.fillTriangle(width - 120, height - 75, width - 120, height - 125, width - 165, height - 100);
+        Utils.fillTriangle(width - 95, height - 175, width - 155, height - 175, width - 125, height - 235);
+        Utils.fillTriangle(width - 75, height - 95, width - 75, height - 155, width - 15, height - 125);
+        Utils.fillTriangle(width - 95, height - 70, width - 155, height - 70, width - 125, height - 10);
+        Utils.fillTriangle(width - 180, height - 95, width - 180, height - 155, width - 240, height - 125);
     } else if (isFirstTime) {
         context.fillStyle = "black";
         context.fillRect(0, 0, width, height);
